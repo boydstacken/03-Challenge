221cecc9e8e1c7c1b18a78933cae33e410dc2b94
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-03-25 15:48:57 -0500

    tweaked variables to strings and updated code to get closer to getting random password to generate correctly

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","s","t","u","v","w","x","y","z"]
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","Q","R","S","T","U","V","W","X","Y","Z"]
-var specialCharacters = ["$", "!", "@", "?"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8" ]
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var specialCharacters = "$#@%&!"
+var numbers = "0123456789"
 var possibleCharacters = []
 
 function generatePassword() {
-    console.log ("you clicked the button")
-    var passwordLength = parseInt(prompt("How many characters would you like a for a password?"))
-    if (passwordLength < 8 ){
-      alert("Your password must be greater than 8 characters")
-      return;
-    }
-      if (passwordLength > 128 ){
-        alert("Your password must be less than 128 characters")
-      return;
-    }
-console.log(passwordLength)
-    var lowerCaseConfirm = confirm ("Do you want lowercase letters in your password?")
-    var upperCaseConfirm = confirm ("Do you want upper case characters in your password?")
-    var numbersConfirm = confirm ("Do you want numbers in your password?")
-    var specialCharactersConfirm = confirm ("Do you want special characters in your password?")
-    if(lowerCaseConfirm === true) {
-      console.log(possibleCharacters)
-    possibleCharacters = possibleCharacters +lowerCase
-      console.log(possibleCharacters)
-    }
-   if(upperCaseConfirm === true) {
-    possibleCharacters = possibleCharacters.concat(upperCase);
-    }
-    if(numbersConfirm === true) {
-    possibleCharacters = possibleCharacters.concat(numbers);
-    }
-    if(specialCharactersConfirm === true) {
-    possibleCharacters = possibleCharacters.concat(specialCharacters);
-    }
+  console.log("you clicked the button")
+  var passwordLength = parseInt(prompt("How many characters would you like a for a password?"))
+  if (passwordLength < 8) {
+    alert("Your password must be greater than 8 characters")
+    return;
+  }
+  if (passwordLength > 128) {
+    alert("Your password must be less than 128 characters")
+    return;
+  }
+  console.log(passwordLength)
+  var lowerCaseConfirm = confirm("Do you want lowercase letters in your password?")
+  var upperCaseConfirm = confirm("Do you want upper case characters in your password?")
+  var numbersConfirm = confirm("Do you want numbers in your password?")
+  var specialCharactersConfirm = confirm("Do you want special characters in your password?")
+  if (lowerCaseConfirm === true) {
+    console.log(possibleCharacters)
+    possibleCharacters.push(lowerCase);
+    console.log(possibleCharacters)
+  }
+  if (upperCaseConfirm === true) {
+    possibleCharacters.push(upperCase);
+  }
+  if (numbersConfirm === true) {
+    possibleCharacters.push(numbers);
+  }
+  if (specialCharactersConfirm === true) {
+    possibleCharacters.push(specialCharacters);
+  }
 
-
+  let randomPassword = ""
+  let newpossibleCharacters = possibleCharacters.join('')
+  console.log(newpossibleCharacters)
   for (var i = 0; i < passwordLength; i++) {
-    let randomValue = Math.floor(Math.random() * possibleCharacters.length);
-    console.log(possibleCharacters[randomValue];
+    let randomValue = Math.floor(Math.random() * newpossibleCharacters.length);
+    console.log(newpossibleCharacters[randomValue]);
+    randomPassword + newpossibleCharacters.charAt(randomValue);
   }
   //const = [upperCase,numbers,specialCharacters]
-  //console.log(elements.join(''));
+  //console.log(elements.push);
 
 
-    //for (var i = 0; i < randomPassword; i++) {
-    //let randomPassword = passwordLength + possibleCharacters([randomValue].passwordLength);
-    //randomPassword = randomPasswrd +  arr[randomValue]
-  
-  
-  return possibleCharacters;
+  //for (var i = 0; i < randomPassword; i++) {
+  //let randomPassword = passwordLength + possibleCharacters([randomValue].passwordLength);
+  //randomPassword = randomPasswrd +  arr[randomValue]
+
+
+  return randomPassword;
 }
 
 // Write password to the #password input
@@ -59,7 +62,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;  
+  passwordText.value = password;
 
 }
 
